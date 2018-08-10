a778069833483e1ebd6499082ac98ebab065e74b
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-08-10 18:28:28 +0200

    ADDED the OSM map visualization

---
+++ src/app/maps/maps.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import L from 'leaflet';
+
+@Component({
+  selector: 'app-maps',
+  templateUrl: './maps.component.html',
+  styleUrls: ['./maps.component.css']
+})
+
+export class MapsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    let body = {town:"Riccione"};
+
+    console.log(body);
+
+    var osmMap = L.map('osmMap').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> ' +
+      'contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoicHVtcGtpbnNoZWFkIiwiYSI6ImNqa2NuM3l2cDFzdGYzcXA4MmoyZ2dsYWsifQ.FahVhmZj5RODSwGjl5-EaQ'
+    }).addTo(osmMap);
+  }
+
+}
