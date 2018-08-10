beb12a44b7b89950fd4bf1bb0ef82052aa615ea7
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-08-10 16:17:55 +0200

    FIX security vulnerabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MapsComponent } from './maps/maps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapsComponent
   ],
   imports: [
     BrowserModule
