452f816c9ff1a02ae07871a3bf02cce89e3ccc16
Matteo Gabellini <matteo.gabellini2@studio.unibo.it>
2018-11-07 10:28:06 +0100

    REFACTORED the marker style management

--- src/app/core/custom.ts
+++ src/app/core/custom.ts
@@ -7,70 +7,78 @@ interface Offset{
 }
 export class Custom {
 
-  public static readonly userColor : string = '#86cbff';
-  public static readonly serverColor: string = '#87cd78';
-  public static readonly fetchedColor: string = '#ff8f83';
-  public static readonly positionColor: string = '#ee00ee';
+  // private static readonly defaultSize : string = '1rem';
 
-  public static readonly defaultBorderColor: string = '#222222';
+  // public static readonly userColor : string = '#86cbff';
+  // public static readonly serverColor: string = '#87cd78';
+  // public static readonly fetchedColor: string = '#ff8f83';
+  // public static readonly positionColor: string = '#ee00ee';
+  //
+  // public static readonly defaultBorderColor: string = '#222222';
+  //
+  // private static readonly userMarkerBorderValue: string = '2px solid ' + Custom.defaultBorderColor;
+  // private static readonly serverMarkerBorderValue: string = '2px solid ' + Custom.defaultBorderColor;
+  // private static readonly fetchedMarkerBorderValue: string = '3px solid ' + Custom.defaultBorderColor;
+  // private static readonly positionMarkerBorderValue: string = '2px solid ' + Custom.defaultBorderColor;
+  //
+  // private static readonly defaultBorderRadius: string = '' + Custom.defaultSize + ' '  + Custom.defaultSize + ' 0';
+  // private static readonly userPositionBorderRadius: string = Custom.defaultSize;
+  //
+  // private static readonly fetchedMarkerOffset: Offset = { left: '-1.15em', top: '-1.15em' };
+  // private static readonly defaultMarkerOffset: Offset = { left: '-1.0em', top: '-1.0em' };
 
-  private static readonly fetchedMarkerBorderValue: string = '3px solid ' + Custom.defaultBorderColor;
-  private static readonly userMarkerBorderValue: string = '2px solid ' + Custom.defaultBorderColor;
-  private static readonly serverMarkerBorderValue: string = '2px solid ' + Custom.defaultBorderColor;
-  private static readonly positionMarkerBorderValue: string = '2px solid ' + Custom.defaultBorderColor;
 
-  private static readonly fetchedMarkerOffset: Offset = { left: '-1.15em', top: '-1.15em' };
-  private static readonly defaultMarkerOffset: Offset = { left: '-1.0em', top: '-1.0em' };
+
   private static readonly iconAnchor : PointExpression = [0, 12];
   private static readonly popupAnchor : PointExpression = [0, -18];
-  private static readonly defaultSize : string = '1rem';
 
-  private static readonly defaultBorderRadius: string = '' + Custom.defaultSize + ' '  + Custom.defaultSize + ' 0';
-  private static readonly userPositionBorderRadius: string = Custom.defaultSize;
-
-  static readonly  setMarkerColor = (color: string, border: string, radius: string, offset: Offset) => `
-    background-color: ${color};
-    width: ${Custom.defaultSize};
-    height: ${Custom.defaultSize};
-    display: block;
-    left: ${offset.left};
-    top: ${offset.top};
-    position: relative;
-    border-radius: ${radius};
-    transform: rotate(45deg);
-    border: ${border}
-  `;
+  // static readonly  setMarkerStyle = (color: string, border: string, radius: string, offset: Offset) => `
+  //   background-color: ${color};
+  //   width: ${Custom.defaultSize};
+  //   height: ${Custom.defaultSize};
+  //   display: block;
+  //   left: ${offset.left};
+  //   top: ${offset.top};
+  //   position: relative;
+  //   border-radius: ${radius};
+  //   transform: rotate(45deg);
+  //   border: ${border}
+  // `;
 
   public static readonly userMarkerIcon : Leaflet.DivIcon = Leaflet.divIcon({
     className: "user-marker-pin",
     iconAnchor: Custom.iconAnchor,
     // iconSize: [24, 24],
     popupAnchor: Custom.popupAnchor,
-    html: `<span style="${Custom.setMarkerColor(Custom.userColor,Custom.userMarkerBorderValue, Custom.defaultBorderRadius, Custom.defaultMarkerOffset)}" />`
+    //html: `<span style="${Custom.setMarkerStyle(Custom.userColor,Custom.userMarkerBorderValue, Custom.defaultBorderRadius, Custom.defaultMarkerOffset)}" />`
+    html: `<span class="marker user-added-marker" />`
   });
 
   public static readonly serverMarkerIcon : Leaflet.DivIcon = Leaflet.divIcon({
-    className: "user-marker-pin",
+    className: "server-marker-pin",
     iconAnchor: Custom.iconAnchor,
     // iconSize: [24, 24],
     popupAnchor: Custom.popupAnchor,
-    html: `<span style="${Custom.setMarkerColor(Custom.serverColor, Custom.serverMarkerBorderValue, Custom.defaultBorderRadius, Custom.defaultMarkerOffset)}" />`
+    //html: `<span style="${Custom.setMarkerStyle(Custom.serverColor, Custom.serverMarkerBorderValue, Custom.defaultBorderRadius, Custom.defaultMarkerOffset)}" />`
+    html: `<span class="marker server-marker" />`
   });
 
   public static readonly fetchedMarkerIcon : Leaflet.DivIcon = Leaflet.divIcon({
-    className: "user-marker-pin",
+    className: "fetched-marker-pin",
     iconAnchor: Custom.iconAnchor,
     // iconSize: [42, 42],
     popupAnchor: Custom.popupAnchor,
-    html: `<span style="${Custom.setMarkerColor(Custom.fetchedColor, Custom.fetchedMarkerBorderValue, Custom.defaultBorderRadius, Custom.fetchedMarkerOffset)}" />`
+    //html: `<span style="${Custom.setMarkerStyle(Custom.fetchedColor, Custom.fetchedMarkerBorderValue, Custom.defaultBorderRadius, Custom.fetchedMarkerOffset)}" />`
+    html: `<span class="marker fetched-marker" />`
   });
 
   public static readonly positionMarkerIcon : Leaflet.DivIcon = Leaflet.divIcon({
-    className: "user-marker-pin",
+    className: "position-marker-pin",
     iconAnchor: Custom.iconAnchor,
     // iconSize: [42, 42],
     popupAnchor: Custom.popupAnchor,
-    html: `<span style="${Custom.setMarkerColor(Custom.positionColor, Custom.positionMarkerBorderValue, Custom.userPositionBorderRadius, Custom.defaultMarkerOffset)}" />`
+    //html: `<span style="${Custom.setMarkerStyle(Custom.positionColor, Custom.positionMarkerBorderValue, Custom.userPositionBorderRadius, Custom.defaultMarkerOffset)}" />`
+    html: `<span class="marker position-marker" />`
   });
 
   public static readonly userMarker =
